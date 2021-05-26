b87e088136671dd3cacdbffc10e8282a1172db1a
Roger Ye <z.rogerye@gmail.com>
2021-05-26 00:07:02 -0500

    Add files via upload
    fixed the lightshot command

---
+++ lightshotRandom.py
@@ -0,0 +1,35 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import urllib
+from urllib.request import Request
+from lxml import html
+import requests
+import random
+import urllib.request
+import requests
+
+
+def lightshot():
+    url = 'https://prnt.sc/'
+    url += str(random.randint(0, 999999))
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html_page = urlopen(req).read()
+    soup = BeautifulSoup(html_page, features="html.parser")
+    sendImg = None
+    for img in soup.findAll('img'):
+        s = img.get('src')
+        if s.startswith("https://i.imgur.com/"):
+            sendImg = s
+
+    if sendImg is None:
+        return 'qiqi says this image has committed nonexistence'
+
+    if sendImg is not None:
+        url = sendImg
+        r = requests.get(url)
+        # print(r.url)
+        bruh = r.url
+        if bruh == 'https://i.imgur.com/removed.png':
+            return 'qiqi says this image has committed nonexistence'
+        if bruh != 'https://i.imgur.com/removed.png':
+            return bruh
